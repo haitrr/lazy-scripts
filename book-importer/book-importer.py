1bf622cd3ac295268b561bf3145ac009bdb07bd2
vroyibg <vroyibg@gmail.com>
2020-05-08 22:54:32 +0700

    change api for book importer

--- book-importer/book-importer.py
+++ book-importer/book-importer.py
@@ -1,14 +1,17 @@
 import bs4
 import requests
+import time
 
 userName = "***"
 password = "*****"
 rootUrl = 'https://www.biquge.com.cn'
-begin = "/book/30594/715187.html"
-num = 100
+begin = "/book/30594/974624.html"
+num = 40
 current = 0
+headers = {
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
 
-login = requests.post("http://lwtapi.hai2cs.com/api/user/login",
+login = requests.post("https://lwt-api.haitran.dev/api/user/login",
                       json={"username": userName, "password": password})
 if login.status_code != 200:
     print("Failed to login")
@@ -17,18 +20,26 @@ print("Login success")
 token = login.json()["token"]
 link = rootUrl+begin
 while current < num:
-    response = requests.get(link)
+    for i in range(10):
+        try:
+            print("try to fetch page: time {count}".format(count=i + 1))
+            response = requests.get(link, headers=headers, timeout=3)
+            print("fetch page success")
+            break
+        except:
+            print("failed to fetch page, wait for 5 seconds")
+            time.sleep(5)
     if(response.status_code != 200):
-      print("Can not get book")
-      print(link)
-      exit(0)
+        print("Can not get book")
+        print(link)
+        exit(0)
     soup = bs4.BeautifulSoup(response.content, 'lxml')
     for br in soup.find_all("br"):
         br.replace_with("\n")
     title = soup.select(".bookname>h1")[0].getText()
     print("Importing "+title)
     content = soup.select("#content")[0].getText()
-    result = requests.post('http://lwtapi.hai2cs.com/api/text', json={
+    result = requests.post('https://lwt-api.haitran.dev/api/text', json={
         "title": title,
         "content": content,
         "language": 2
