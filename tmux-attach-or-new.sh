75aca6c163cc314d08cb562e9acccddd84ba118f
Hai Tran <yvroyibg@gmail.com>
2022-08-21 07:27:35 +0000

    fix dirname for tmux attach script

--- tmux-attach-or-new.sh
+++ tmux-attach-or-new.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # Attach to the first tmu session that has no attached clients.
 # If there are no unattached sessions, then create a new session.
-tmux attach -t $(./tmux-first-unattached-session.sh) 2> /dev/null || tmux
+tmux attach -t $($(dirname $0)/./tmux-first-unattached-session.sh) 2> /dev/null || tmux
